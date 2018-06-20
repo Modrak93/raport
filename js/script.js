e2dc792d9c99062031dd49863df373b4fe01926a
Przemyslaw Modrakowski <prz.modrakowski@gmail.com>
2018-06-20 12:54:57 +0200

    files of completed website

---
+++ js/script.js
@@ -0,0 +1,137 @@
+    // -------------------------------------------------------------
+    //  Slick Slider
+    // -------------------------------------------------------------  
+
+    (function() {
+      
+        $(".rec-slider").slick({
+            infinite: true,
+            dots: true,
+            slidesToShow: 1,
+            arrows: false,
+            autoplay:true,
+            autoplaySpeed:5000,
+            slidesToScroll: 1,       
+        }); 
+        
+                   
+
+    }());
+
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 57)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+    
+    window.onscroll = function() {
+        if ($(this).width() > 320) {
+  growShrinkLogo()
+        }
+};
+    
+function growShrinkLogo() {
+  var Logo = document.getElementById("navbar-logo")
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 5) {
+    Logo.style.width = '16px';
+  } else {
+    Logo.style.width = '80px';
+  }
+}
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 57
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+      $("#navbar-logo").attr("src","img/logo-color.svg");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+      $("#navbar-logo").attr("src","img/logo-white.svg");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Scroll reveal calls
+  window.sr = ScrollReveal();
+  sr.reveal('.sr-icons', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 200);
+  sr.reveal('.sr-button', {
+    duration: 1000,
+    delay: 200
+  });
+  sr.reveal('.sr-contact', {
+    duration: 600,
+    scale: 0.3,
+    distance: '0px'
+  }, 300);
+
+  // Magnific popup calls
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1]
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+    }
+  });
+    
+    window.onscroll = function() {
+        if ($(this).width() > 320) {
+  growShrinkLogo()
+        }
+};
+
+function growShrinkLogo() {
+  var Logo = document.getElementById("Logo")
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 5) {
+    Logo.style.width = '36px';
+  } else {
+    Logo.style.width = '80px';
+  }
+}
+  
+
+                                
+
+    
+
+})(jQuery); // End of use strict
+
+
+
+
+    
